3827537e31a2dd20f5a0c9c4647fb16396466228
Douglas <douglbre@oregonstate.edu>
2018-01-23 11:28:08 -0800

    added edit page

--- examples/message_board/board/urls.py
+++ examples/message_board/board/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path(r'', views.index, name='index'),
     path(r'clear_all', views.clear_all, name='clear_all'),
+    path(r'edit/<int:msg_id>/', views.edit_msg, name='edit_msg'),
 ]
